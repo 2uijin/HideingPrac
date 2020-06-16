ff5c33acdda8c467404f02111fe0fe1fda145d22
2uijin <49094082+2uijin@users.noreply.github.com>
2020-06-16 14:37:38 +0900

    초기 커밋.

---
+++ HideingPrac/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HideingPrac
+{
+    class Program
+    {
+        class Prants {s
+            
+            }
+        }
+
+        class Chlid {
+            public Chlid() { 
+            
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
